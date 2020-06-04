93ffc584a92ff727060969bc5bc443465b63dcf8
Hasan <hasan.akkas@web.de>
2020-06-04 19:29:16 +0200

    navbar last version

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,17 +7,17 @@ function Header() {
     return (
         <div>
             <Navbar bg="dark" variant="dark" expand="md" fixed="top">
-                <Navbar.Brand href="/">FBW-8</Navbar.Brand>
+                <Navbar.Brand href="/" style={{fontWeight: 'bold', marginRight: '40px'}}>FBW-8</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="mr-auto">
-                        <Link to="/" className="mr-2" >Home</Link>
-                        <Link to="/how_you_doing" className="mr-2" >How you doing</Link>
-                        <Link to="/wawawiwa" className="mr-2" >Wawawiwa</Link>
-                        <Link to="/albatros" className="mr-2" >Albatros</Link>
-                        <Link to="/tom&jerry" className="mr-2" >Tom & Jerry</Link>
-                        <Link to="/be_guys" className="mr-2" >BE Guy's</Link>
-                        <Link to="/vins" className="mr-2" >Vins</Link>
+                        <Link to="/" className="link" >Home</Link>
+                        <Link to="/how_you_doing" className="link" >How you doing</Link>
+                        <Link to="/wawawiwa" className="link" >Wawawiwa</Link>
+                        <Link to="/albatros" className="link" >Albatros</Link>
+                        <Link to="/tom&jerry" className="link" >Tom & Jerry</Link>
+                        <Link to="/be_guys" className="link" >BE Guy's</Link>
+                        <Link to="/vins" className="link" >Vins</Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
